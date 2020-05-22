ad2b89da1710bbc73f5520713678e67d2d60af4e
Eric <ericgmicaela@gmail.com>
2020-05-22 14:03:46 -0300

    Versão 2.0

--- Desafio049.py
+++ Desafio049.py
@@ -3,4 +3,4 @@ coeficiente = int(input("Informe o operando: "))
 operando = int(input("Tabuada até qual operador: "))
 print(f"Tabaada do numero: {coeficiente}")
 for c in range(0, operando):
-    print(f"{coeficiente}*{c}={c*coeficiente}")
+    print(f"{coeficiente:0>2}*{c:0>2}={c*coeficiente:0>2}")
